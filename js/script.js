1f1ac23bd594b5c661a1d81ee5bebc960c5df5c0
EJ Serafin <edgardo@chainblx.com>
2019-11-14 22:52:17 -0800

    quotes array created

--- js/script.js
+++ js/script.js
@@ -10,7 +10,36 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+var quotes= [
+  {
+    quote: "Frankly my dear, I don't give a damn",
+    source: "Rhett Butler",
+    year: 1939
+  },
+  {
+    quote: "I'm the king of the world!",
+    source: "Leonardo DiCaprio",
+    citation: "Titanic",
+    year: 1997
+  },
+  {
+    quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
+    source: "Robin Williams",
+    citation: "Dead Poets Society",
+    year: 1989
+  },
+  {
+    quote: "My mama always said life was like a box of chocolates. You never know what you're gonna get.",
+    source: "Tom Hanks",
+    citation: "Forrest Gump",
+    year: 1994
+  },
+  {
+    quote: "You talking to me?",
+    source: "Robert DeNiro",
+    citation: "Taxi Driver",
+  }
+];
 
 
 /***
